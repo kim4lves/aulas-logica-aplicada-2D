78d8b092da7258fc9a82dda7e00ff0dd4e5f3cf2
kim4lves <kim4lves@gmail.com>
2023-05-08 11:51:16 -0300

    apaguei tudo

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // laço enquanto - while
-        int contador = 0;
-        while (contador < 10) {
-            // processos
-            System.out.println("Contador = " + contador);
-            contador = contador + 1;
-        }
+        // Aula inicial de Orientação à Objetos
 
-        // laço faça enquanto - do while
-        int cont = 1;
-        do {
-            System.out.println(cont);
-            cont++; //incremento
-        }while(cont <= 10);
-
-        // laço par faça for
-        for(int x=0; x<10; x++){
-            System.out.println(x);
-
-        }
     }
 }
