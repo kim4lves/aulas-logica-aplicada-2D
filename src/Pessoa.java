c39602a65ef1c2617bb0a8497e5dea005a85e40b
kim4lves <kim4lves@gmail.com>
2023-10-05 10:45:53 -0300

    viagens 2d

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,20 +2,23 @@ public class Pessoa {
     // Atributo
     String nome;
     String sobrenome;
+
     // metodos
-    public void falar(){
+    public void falar() {
         System.out.println("falei");
     }
+
     // Sobrecarga do metodo falar()
-    public String falar(String volume) { return  "falei " + volume; }
+    public String falar(String volume) {
+        return "falei " + volume;
+    }
+
     // Construtor
-    Pessoa(){
+    Pessoa() {
 
     }
-    Pessoa(String _nome) {
-        this.nome=_nome;
-        public void comer(){
+        public void comer() {
             System.out.println("Pessoa Comeu ! ! !");
         }
+
     }
-}
---
+++ src/Solo.java
@@ -0,0 +1,11 @@
+public class Solo {
+    //
+     String tipoSolo;
+     boolean fertil;
+     String cor;
+     // Método
+    public Planta gerarPlanta(Semente semente ){
+        return new Planta(semente);
+    }
+
+}
